be2bb3665a785a8d4caa576d0d68367a35bb8203
sara2086 <you@gmail.com>
2023-04-03 11:40:41 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - bytes
+ * @s: pointer to char
+ * @accept: pointer to char
+ * Return: NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int l;
+
+	while (*s)
+	{
+		for (l = 0; accept[l]; l++)
+		{
+			if (*s == accept[l])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
